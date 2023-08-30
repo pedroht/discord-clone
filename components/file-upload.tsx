c828cf570d6b1cf1272e99f7ce8e770b42246aec
pedroht <torres.pedrohen@gmail.com>
2023-08-30 00:26:18 -0300

    feat: create file upload component

---
+++ components/file-upload.tsx
@@ -0,0 +1,42 @@
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+interface FileUploadProps {
+  endpoint: "messageFile" | "serverImage";
+  value: string;
+  onChange(url?: string): void;
+}
+
+export function FileUpload({ endpoint, value, onChange }: FileUploadProps) {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          type="button"
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.error(error);
+      }}
+    />
+  );
+}
