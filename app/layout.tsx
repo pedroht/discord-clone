67d08d542e6ee48a59a3ee29777232e1b1b077a4
pedroht <torres.pedrohen@gmail.com>
2023-08-30 15:48:25 -0300

    feat: add create server modal with zustand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -27,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
--- components/navigation/action.tsx
+++ components/navigation/action.tsx
@@ -1,11 +1,20 @@
-import { ActionTooltip } from "@/components/action-tooltip";
+"use client";
+
 import { Plus } from "lucide-react";
 
+import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
+
 export function NavigationAction() {
+  const { onOpen } = useModal();
+
   return (
     <div>
       <ActionTooltip side="right" align="center" label="Add a server">
-        <button className="group flex items-center">
+        <button
+          onClick={() => onOpen("createServer")}
+          className="group flex items-center"
+        >
           <div className="flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
             <Plus
               className="group-hover:text-white transition text-emerald-500"
