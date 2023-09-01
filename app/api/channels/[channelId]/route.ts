879c95d9cc2c76fc13c325625b9b2f251737a84a
pedroht <torres.pedrohen@gmail.com>
2023-09-01 00:15:47 -0300

    feat: add edit channel modal functionality

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -9,6 +9,63 @@ interface ParamsProps {
   };
 }
 
+export async function PATCH(request: Request, { params }: ParamsProps) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { name, type } = await request.json();
+    const { searchParams } = new URL(request.url);
+    const serverId = searchParams.get("serverId");
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (name === "general") {
+      return new NextResponse("Name cannot be 'genearl'", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profile_id: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function DELETE(request: Request, { params }: ParamsProps) {
   try {
     const profile = await currentProfile();
@@ -21,7 +78,7 @@ export async function DELETE(request: Request, { params }: ParamsProps) {
     const serverId = searchParams.get("serverId");
 
     if (!serverId) {
-      return new NextResponse("Server ID missing", { status: 401 });
+      return new NextResponse("Server ID missing", { status: 400 });
     }
 
     const server = await db.server.update({
