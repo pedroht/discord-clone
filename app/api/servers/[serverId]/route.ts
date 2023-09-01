478ac285beb754b47607688ecd0e2edc4e02268a
pedroht <torres.pedrohen@gmail.com>
2023-08-31 22:31:23 -0300

    feat: add delete server functionality

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-interface PatchProps {
+interface ParamsProps {
   params: {
     serverId: string;
   };
 }
 
-export async function PATCH(request: Request, { params }: PatchProps) {
+export async function PATCH(request: Request, { params }: ParamsProps) {
   try {
     if (!params.serverId) {
       return new NextResponse("Server ID Missing", { status: 400 });
@@ -37,6 +37,28 @@ export async function PATCH(request: Request, { params }: PatchProps) {
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(request: Request, { params }: ParamsProps) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profile_id: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
