7eac9e700bef7b81175f0a68da0076a652a74198
pedroht <torres.pedrohen@gmail.com>
2023-08-31 22:11:37 -0300

    feat: add create channels funcitonality

---
+++ app/api/channels/route.ts
@@ -0,0 +1,56 @@
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function POST(request: Request) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { name, type } = await request.json();
+    const { searchParams } = new URL(request.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (name.toLowerCase() === "general") {
+      return new NextResponse("Name cannot be 'general'", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profile_id: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          create: {
+            name,
+            type,
+            profile_id: profile.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("CHANNELS_POST", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
