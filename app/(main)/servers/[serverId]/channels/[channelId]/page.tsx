c8d58c3955bea8acbdfec8747bd4547e6546efd3
pedroht <torres.pedrohen@gmail.com>
2023-09-01 17:15:18 -0300

    feat: add chat header to channels

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,11 +1,48 @@
-import { PropsWithChildren } from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-interface ChannelIdProps extends PropsWithChildren {}
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-export default async function ChannelIdPage({ children }: ChannelIdProps) {
+interface ChannelIdProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+export default async function ChannelIdPage({ params }: ChannelIdProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params?.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      server_id: params.serverId,
+      profile_id: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect(`/`);
+  }
+
   return (
-    <div>
-      <h1>ChannelID Page</h1>
+    <div className="bg-white dark:bg-[#313338] flex flex-col w-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.server_id}
+        type="channel"
+      />
     </div>
   );
 }
